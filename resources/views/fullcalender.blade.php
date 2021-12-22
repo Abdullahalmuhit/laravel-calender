8bdbc57638f0f01c8bc4af5f17850d072638288d
mahbub <m.alam3330@gmail.com>
2021-12-22 13:15:06 +0600

    first commit

---
+++ resources/views/fullcalender.blade.php
@@ -0,0 +1,12 @@
+<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.css"/>
+<body>{!! $calendar->calendar() !!}</body>
+                   
+                    
+       
+
+<script src="//cdnjs.cloudflare.com/ajax/libs/moment.js/2.9.0/moment.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/fullcalendar/2.2.7/fullcalendar.min.js"></script>
+
+
+{!! $calendar->script() !!}
+
